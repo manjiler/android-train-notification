b2b4c922c0860764e09f534fbe3672a3c3bd8236
Manoj Srivatsav <manoj.srivatsav@gmail.com>
2014-01-01 01:07:42 +0530

    Utility class for date time conversion and repetition string conversion.

---
+++ src/com/ms/android/utils/DateTimeUtils.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ms.android.utils;
+
+/**
+ * @author Manoj Srivatsav
+ *
+ * Nov 30, 2013
+ */
+public class DateTimeUtils 
+{
+	
+	public static String getNthDayOfMonth(long timestamp)
+	{
+		return null;
+	}
+	
+	
+
+}
